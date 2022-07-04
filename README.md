0af6f584f14acb59e63b27f832d02f5ef9178b18
marmooo <marmooo@users.noreply.github.com>
2022-07-04 09:17:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# icon-search
+
+[Icon Search](https://marmooo.github.io/icon-search). 100+ icon sets with
+100,000 icons.
+
+## Installation
+
+install [marmooo/icon-db](https:github.com/marmooo/icon-db)
+
+## Build
+
+```
+bash build.sh
+```
---
+++ src/index.js
@@ -0,0 +1,163 @@
+function loadConfig() {
+  if (localStorage.getItem("darkMode") == 1) {
+    document.documentElement.dataset.theme = "dark";
+  }
+}
+
+function toggleDarkMode() {
+  if (localStorage.getItem("darkMode") == 1) {
+    localStorage.setItem("darkMode", 0);
+    delete document.documentElement.dataset.theme;
+  } else {
+    localStorage.setItem("darkMode", 1);
+    document.documentElement.dataset.theme = "dark";
+  }
+}
+
+function initSuggest(json) {
+  const obj = document.getElementById("tags");
+  json.forEach((tag) => {
+    tags.set(tag, true);
+    const option = document.createElement("option");
+    option.textContent = tag;
+    obj.appendChild(option);
+  });
+}
+
+function initCollections() {
+  fetch("/icon-db/collections.json")
+    .then((response) => response.json())
+    .then((json) => {
+      json.forEach((iconSet) => {
+        collections.set(iconSet.name, iconSet);
+        delete iconSet.name;
+      });
+    });
+}
+
+function initTags() {
+  fetch("/icon-db/tags.json")
+    .then((response) => response.json())
+    .then((json) => initSuggest(json));
+}
+
+function showIconDetails(svg, icon) {
+  const iconTags = icon[1];
+  const iconSetName = icon[2];
+  showIconSetDetails(iconTags, iconSetName);
+  const selectedIcon = svg.cloneNode(true);
+  selectedIcon.removeAttribute("role");
+  selectedIcon.removeAttribute("data-bs-toggle");
+  selectedIcon.removeAttribute("data-bs-target");
+  selectedIcon.removeAttribute("aria-controls");
+  selectedIcon.setAttribute("width", 128);
+  selectedIcon.setAttribute("height", 128);
+  selectedIcon.onclick = null;
+  const selectedIconParent = document.getElementById("selectedIcon");
+  selectedIconParent.firstChild.replaceWith(selectedIcon);
+}
+
+function showIconSetDetails(iconTags, iconSetName) {
+  const iconSet = collections.get(iconSetName);
+  document.getElementById("iconTags").textContent = iconTags;
+  document.getElementById("name").textContent = iconSetName;
+  document.getElementById("name").href = iconSet.homepage;
+  document.getElementById("license").textContent = iconSet.license;
+  document.getElementById("license").href = iconSet.licenseUrl;
+  document.getElementById("author").textContent = iconSet.author;
+}
+
+function fetchIcons(tag) {
+  if (tags.has(tag) === false) {
+    document.getElementById("noTags").classList.remove("invisible");
+    return;
+  }
+  document.getElementById("noTags").classList.add("invisible");
+
+  const previewSize =
+    document.getElementById("previewSize").value.split("x")[0];
+  const result = document.getElementById("result");
+  const div = document.createElement("div");
+  result.replaceChild(div, result.firstChild);
+
+  // TODO: ReadableStream
+  return fetch(`/icon-db/json/${tag}.json`)
+    .then((response) => response.json())
+    .then((icons) => {
+      const domParser = new DOMParser();
+      icons.forEach((icon, i) => {
+        const svgText = icon[0];
+        // benchmark: https://www.measurethat.net/Benchmarks/Show/14659
+        const obj = domParser.parseFromString(svgText, "image/svg+xml");
+        const svg = obj.documentElement;
+        icons[i][0] = null;
+        svg.setAttribute("width", previewSize);
+        svg.setAttribute("height", previewSize);
+        svg.setAttribute("role", "button");
+        svg.setAttribute("data-bs-toggle", "offcanvas");
+        svg.setAttribute("data-bs-target", "#details");
+        svg.setAttribute("aria-controls", "details");
+        svg.onclick = () => {
+          showIconDetails(svg, icons[i]);
+        };
+        div.appendChild(svg);
+      });
+    });
+}
+
+function uniqIds() {
+  const result = document.getElementById("result");
+  [...result.firstChild.children].forEach((svg) => {
+    svg.querySelectorAll("[id]").forEach((idElement) => {
+      const id = idElement.id;
+      const uniqId = Math.random().toString(16).slice(2);
+      const idRegExp = new RegExp(`url\\(#${id}\\);?`, "g");
+      idElement.id = uniqId;
+      [...svg.getElementsByTagName("*")].forEach((e) => {
+        [...e.attributes].forEach((attr) => {
+          const name = attr.name;
+          const value = attr.value;
+          if (name == "xlink:href" && value.startsWith("#")) {
+            e.setAttribute(name, `#${uniqId}`);
+          } else {
+            const newValue = value.replace(idRegExp, `url(#${uniqId});`);
+            if (value != newValue) {
+              e.setAttribute(name, newValue);
+            }
+          }
+        });
+      });
+    });
+  });
+}
+
+function search() {
+  const obj = document.getElementById("searchText");
+  const tag = obj.value.split(" ")[0];
+  obj.blur();
+  obj.focus();
+  fetchIcons(tag).then(() => {
+    uniqIds();
+  });
+}
+
+loadConfig();
+const tags = new Map();
+const collections = new Map();
+initTags();
+initCollections();
+new bootstrap.Offcanvas(document.getElementById("details"));
+
+document.getElementById("toggleDarkMode").onclick = toggleDarkMode;
+document.getElementById("search").onclick = search;
+document.getElementById("searchText").onkeydown = (event) => {
+  if (event.key == "Enter") search();
+};
+document.getElementById("previewSize").onchange = (event) => {
+  const previewSize = event.target.value.split("x")[0];
+  const result = document.getElementById("result");
+  [...result.firstChild.children].forEach((svg) => {
+    svg.setAttribute("width", previewSize);
+    svg.setAttribute("height", previewSize);
+  });
+};
