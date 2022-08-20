f5feb787358b2fefde73f51ddf3ad9ebd9ace504
marmooo <marmooo@users.noreply.github.com>
2022-08-20 16:38:42 +0900

    performance update

--- src/worker.js
+++ src/worker.js
@@ -1,23 +1,37 @@
-function uniqIds(doc) {
-  doc.querySelectorAll("[id]").forEach((idElement) => {
+function cleanup(doc) {
+  const idElements = [];
+  const urlAttributes = [];
+  const styleElements = [];
+  [...doc.getElementsByTagName("*")].forEach((e) => {
+    for (const [name, value] of Object.entries(e.attributes)) {
+      if (name == "id") idElements.push(e);
+      if (e.tagName == "style") styleElements.push(e);
+      if (value.startsWith("#")) urlAttributes.push([e, name, value]);
+      if (value.includes("url")) urlAttributes.push([e, name, value]);
+    }
+  });
+  uniqIds(doc, idElements, urlAttributes);
+  uniqClasses(doc, styleElements);
+}
+
+function uniqIds(doc, idElements, urlAttributes) {
+  idElements.forEach((idElement) => {
     const id = idElement.id;
     const uniqId = "id-" + Math.random().toString(16).slice(2);
     const idRegExp = new RegExp(`url\\(#${id}\\)`, "g");
     idElement.setAttribute("id", uniqId);
-    [...doc.getElementsByTagName("*")].forEach((e) => {
-      for (const [name, value] of Object.entries(e.attributes)) {
-        switch (name) {
-          case "href": // SVG 2.0
-          case "xlink:href": // SVG 1.1
-            if (value == `#${id}`) {
-              e.setAttribute(name, `#${uniqId}`);
-            }
-            break;
-          default: {
-            const newValue = value.replace(idRegExp, `url(#${uniqId})`);
-            if (value != newValue) {
-              e.setAttribute(name, newValue);
-            }
+    urlAttributes.forEach(([e, name, value]) => {
+      switch (name) {
+        case "href": // SVG 2.0
+        case "xlink:href": // SVG 1.1
+          if (value == `#${id}`) {
+            e.setAttribute(name, `#${uniqId}`);
+          }
+          break;
+        default: {
+          const newValue = value.replace(idRegExp, `url(#${uniqId})`);
+          if (value != newValue) {
+            e.setAttribute(name, newValue);
           }
         }
       }
@@ -26,17 +40,21 @@ function uniqIds(doc) {
   return doc;
 }
 
-function uniqClasses(doc) {
-  const styles = doc.querySelectorAll("style");
-  if (styles) {
+function uniqClasses(doc, styleElements) {
+  if (styleElements.length > 0) {
     const regexp = new RegExp("(\.[^}]+\})", "g");
     const uniqId = "id-" + Math.random().toString(16).slice(2);
     const svg = doc.querySelector("svg");
     svg.setAttribute("id", uniqId);
-    styles.forEach((style) => {
-      style.textContent = style.textContent.replace(regexp, `#${uniqId} $1`);
+    styleElements.forEach((styleElement) => {
+      const style = styleElement.textContent;
+      const newStyle = styles.replace(regexp, `#${uniqId} $1`);
+      if (style != newStyle) {
+        styleElement.textContent = newStyle;
+      }
     });
   }
+  return doc;
 }
 
 function setPreviewInfo(doc, previewSize) {
@@ -55,8 +73,7 @@ importScripts("node-html-parser.js");
 self.addEventListener("message", (e) => {
   const [svgText, pos, previewSize] = e.data;
   const doc = HTMLParser.parse(svgText);
-  uniqIds(doc);
-  uniqClasses(doc);
+  cleanup(doc);
   setPreviewInfo(doc, previewSize);
   postMessage([doc.toString(), pos]);
 });
