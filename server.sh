b4de4dc16273fb5fb33763ef8ab2163fdf97b6b3
marmooo <marmooo@users.noreply.github.com>
2025-04-02 00:04:55 +0900

    remove unused file

--- server.sh
+++
@@ -1,6 +0,0 @@
-# sudo apt install mkcert
-# mkcert -install
-# mkdir ~/.ssl && cd ~/.ssl
-# mkcert localhost & cd ..
-# sudo npm install http-server -g
-http-server --ssl --cert ~/.ssl/localhost.pem --key ~/.ssl/localhost-key.pem
